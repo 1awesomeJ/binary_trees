e9b281b10213a142f8a17963253c3fa5562f036c
OMOJOLA JOSHUA DAMILOLA <omojolajoshua@gmail.com>
2023-04-15 07:58:29 +0000

    task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts a node as the left child of another node.
+ *
+ * @value: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * Return: a pointer to the newly created node.
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	if (parent == NULL)
+		return (NULL);
+	binary_tree_t *ret = malloc(sizeof(binary_tree_t));
+
+	if (ret == NULL)
+		return (NULL);
+	ret->parent = parent;
+	ret->n = value;
+	ret->left = NULL;
+	ret->right = NULL;
+
+	if (parent->left == NULL)
+		parent->left = ret;
+	else
+	{
+		ret->left = parent->left;
+		parent->left->parent = ret;
+		parent->left = ret;
+	}
+	return (ret);
+}
